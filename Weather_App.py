e04b482506de42231e85bdf1d437c107688907ad
Kelly Ortega <InvaderLynn10@gmail.com>
2023-08-24 10:43:53 -0400

    added more error handling

--- Weather_App.py
+++ Weather_App.py
@@ -21,58 +21,69 @@ def forecast_data(): # returns forecast and city data as json.
         # Search for both town and state in the dataframe
         towndata = df[(df['City'].str.contains(town, case=False, na=False)) & (df['State'] == state_initial.upper())]
 
-    latitude = towndata['Latitude'].values[0]
-    longitude = towndata['Longitude'].values[0]
-    location_url = f"https://api.weather.gov/points/{latitude},{longitude}"
-
-    response = requests.get(location_url)
-    data = response.json
-
-    if response.status_code == 200:
-        data = response.json()
+    if(towndata.size != 0):
         
-        # Make sure data contains the required keys
-        if 'properties' in data and 'gridX' in data['properties'] and 'gridY' in data['properties']:
-            gridX = data['properties']['gridX']
-            gridY = data['properties']['gridY']
-            office = data['properties']['cwa']
-            
-            forecast_url = f"https://api.weather.gov/gridpoints/{office}/{gridX},{gridY}/forecast"
-        else:
-            print("The response JSON does not contain the expected keys.")
-    else:
-        print(f"Request failed with status code: {response.status_code}")
+        latitude = towndata['Latitude'].values[0]
+        longitude = towndata['Longitude'].values[0]
+        location_url = f"https://api.weather.gov/points/{latitude},{longitude}"
 
-    forecast_data = requests.get(forecast_url).json()
+        response = requests.get(location_url)
+        data = response.json
+
+        if response.status_code == 200:
+            data = response.json()
+        
+            # Make sure data contains the required keys
+            if 'properties' in data and 'gridX' in data['properties'] and 'gridY' in data['properties']:
+                gridX = data['properties']['gridX']
+                gridY = data['properties']['gridY']
+                office = data['properties']['cwa']
+            
+                forecast_url = f"https://api.weather.gov/gridpoints/{office}/{gridX},{gridY}/forecast"
+            else:
+                print("The response JSON does not contain the expected keys.")
+                forecast_data, data = None, None
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            forecast_data, data = None, None
+    else:
+        print(f"Invalid City, State Initial, or Zipcode.")
+        forecast_data, data = None, None
+
+    if(forecast_data != None and data != None):
+        forecast_data = requests.get(forecast_url).json()
     return forecast_data, data
 
 def forecast(forecast_data, data): #Returns forcast, requires forecast_data, and city data.
-    
+
         # Ensure the forecast_data contains the expected 'properties' and 'periods' keys
-    if 'properties' in forecast_data and 'periods' in forecast_data['properties']:
-        forecast_periods = forecast_data['properties']['periods']
-        city = data['properties']['relativeLocation']['properties']['city']
-        state = data['properties']['relativeLocation']['properties']['state']
+    if (forecast_data != None and data != None):
+        if 'properties' in forecast_data and 'periods' in forecast_data['properties']:
+            forecast_periods = forecast_data['properties']['periods']
+            city = data['properties']['relativeLocation']['properties']['city']
+            state = data['properties']['relativeLocation']['properties']['state']
         
         # Loop through the forecast periods and print out the first 20 periods (10 days, assuming 2 periods per day)
-        print("-----------------------------------------------" *2 + \
-              f"\nForecast for {city}, {state}:\n" + \
-              "-----------------------------------------------" *2)
-        for period in forecast_periods[:14]:
-            name = period['name']
-            detailed_forecast = period['detailedForecast']
-            temperature = period['temperature']
-            temperature_unit = period['temperatureUnit']
-            wind_speed = period['windSpeed']
-            wind_direction = period['windDirection']
+            print("-----------------------------------------------" *2 + \
+                f"\nForecast for {city}, {state}:\n" + \
+                "-----------------------------------------------" *2)
+            for period in forecast_periods[:14]:
+                name = period['name']
+                detailed_forecast = period['detailedForecast']
+                temperature = period['temperature']
+                temperature_unit = period['temperatureUnit']
+                wind_speed = period['windSpeed']
+                wind_direction = period['windDirection']
             
-            print(f"{name}:")
-            print(f"Temperature: {temperature} {temperature_unit}")
-            print(f"Wind: {wind_speed} {wind_direction}")
-            print(f"Forecast: {detailed_forecast}")
-            print("-----------------------------------------------")  # Separator for clarity
+                print(f"{name}:")
+                print(f"Temperature: {temperature} {temperature_unit}")
+                print(f"Wind: {wind_speed} {wind_direction}")
+                print(f"Forecast: {detailed_forecast}")
+                print("-----------------------------------------------")  # Separator for clarity
+        else:
+            print("Forecast data doesn't contain the expected structure.")
     else:
-        print("Forecast data doesn't contain the expected structure.")
+        print("Location not found.")
 
 
 forecastdata, data = forecast_data()
